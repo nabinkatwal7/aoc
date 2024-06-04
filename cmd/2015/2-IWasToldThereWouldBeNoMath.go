9761205b0fb6a5a277902fd4814a8e0967792343
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-06-04 11:25:20 +0545

    test

--- cmd/2015/2-IWasToldThereWouldBeNoMath.go
+++ cmd/2015/2-IWasToldThereWouldBeNoMath.go
@@ -3,7 +3,50 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Dimensions struct {
+	Length int
+	Width  int
+	Height int
+}
+
+func separateDimensions(data string) []Dimensions {
+	lines := strings.Split(data, "\n")
+
+	var dimensions []Dimensions
+
+	for _, line := range lines {
+		values := strings.Fields(line)
+		individualValues := strings.Split(values[0], "x")
+		
+		l, err := strconv.Atoi(individualValues[0])
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		w, err := strconv.Atoi(individualValues[1])
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		h, err := strconv.Atoi(individualValues[2])
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		dimensions = append(dimensions, Dimensions{l, w, h})
+	}
+	return dimensions
+}
 
 func RunTwo() {
 	input, err := os.ReadFile("inputs/2-IWasToldThereWouldBeNoMath.txt")
@@ -12,5 +55,9 @@ func RunTwo() {
 		panic(err)
 	}
 
-	fmt.Println(string(input))
+	allDimensions := separateDimensions(string(input))
+
+	for _, dim := range allDimensions {
+		fmt.Printf("l: %d, w: %d, h: %d\n", dim.Length, dim.Width, dim.Height)
+	}
 }
