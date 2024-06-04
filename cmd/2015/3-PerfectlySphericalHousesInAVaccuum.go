580af4d586887176328ee47152ab14695f5d8557
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-06-04 14:14:57 +0545

    day 3

--- cmd/2015/3-PerfectlySphericalHousesInAVaccuum.go
+++ cmd/2015/3-PerfectlySphericalHousesInAVaccuum.go
@@ -17,6 +17,7 @@ func RunThree () {
 	input, err := os.ReadFile("inputs/day3.txt")
 
 	var coordinates = []int{0, 0}
+	var robotCoordinates = []int{0, 0}
 	var coordinatesList = []string{"0,0"}
 
 	north := "^"
@@ -31,22 +32,42 @@ func RunThree () {
 	for i := 0; i < len(input); i++ {
 		direction := string(input[i])
 		if direction == north {
-			coordinates[1]++
+			if i % 2==0{
+				coordinates[1]++
+			}else{
+				robotCoordinates[1]++
+			}
 		}
 
 		if direction == south {
-			coordinates[1]--
+			if i % 2==0{
+				coordinates[1]--
+			}else{
+				robotCoordinates[1]--
+			}
 		}
 
 		if direction == west {
-			coordinates[0]--
+			if i % 2==0{
+				coordinates[0]--
+			}else{
+				robotCoordinates[0]--
+			}
 		}
 
 		if direction == east {
-			coordinates[0]++
+			if i % 2==0{
+				coordinates[0]++
+			}else{
+				robotCoordinates[0]++
+			}
 		}
 
-		coordinatesList = append(coordinatesList, fmt.Sprintf("%d,%d", coordinates[0], coordinates[1]))
+		if i % 2==0{
+			coordinatesList = append(coordinatesList, fmt.Sprintf("%d,%d", coordinates[0], coordinates[1]))
+		}else{
+			coordinatesList = append(coordinatesList, fmt.Sprintf("%d,%d", robotCoordinates[0], robotCoordinates[1]))
+		}
 	}
 
 	fmt.Println(countUniqueCoordinates(coordinatesList))
